fd597f91d06a5cdf15c8d0520847ba1069be9386
Jose Alvarez <rosney.sotor@gmail.com>
2022-11-27 00:26:35 -0300

    Se inicia el proyecto final

---
+++ ProyectoFinal/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs')
+
+const port = 3000;
+
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use('/', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        titulo: 'Mi pagina web'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about',{
+        titulo: "Sobre Nosotros"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servido escuchando en el puerto ${port}`);
+});
