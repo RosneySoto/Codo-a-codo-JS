872f59fcb0bcd101c3980ca2b82756d8deec46ff
Jose Alvarez <rosney.sotor@gmail.com>
2023-01-21 09:37:39 -0300

    Se finaliza el CRUD basico Create, Remove, Update, Delete

--- ProyectoFinal/controllers/back.js
+++ ProyectoFinal/controllers/back.js
@@ -37,8 +37,48 @@ const agregarProductoPOST = (req, res) => {
 }
 
 const editarProductoGET = (req, res) => {
-    res.render('editar-producto', {
-        titulo: "Vista del administrador"
+    const id = req.params.id
+    const data = req.body
+    const sql = " SELECT * FROM productos WHERE idProducto = ?";
+    db.query(sql, id, (err, data) => {
+        if(err) throw err
+        if(data.length > 0){
+            res.render('editar-producto', {
+                titulo: "Vista del administrador",
+                productos: data[0]
+            });
+        } else {
+            console.log('ID no encontrado')
+            res.send(`
+                <h1>No existe el producto con id ${id}</h1>
+                <a href="/admin"> Ver listado de productos</a>
+            `)
+        };
+    });
+};
+
+const editarProductoPOST = (req, res) => {
+    const id = req.params.id;
+    const producto = req.body;
+    console.log(producto);
+    const sql = "UPDATE productos SET ? WHERE idProducto = ?";
+
+    db.query(sql, [producto, id], (err, data) => {
+        if(err) throw err
+        console.log(data);
+        console.log(`${data.affectedRows} Registro Actualizado`);
+        res.redirect('/admin');
+    });
+};
+
+const borrarProductoGET = (req, res) => {
+    const id = req.params.id
+
+    const sql = "DELETE FROM productos WHERE idProducto = ?";
+    db.query(sql, id, (err, data) => {
+        if(err) throw err
+        console.log(data.affectedRows + "Registro borrado")
+        res.redirect('/admin');
     });
 };
 
@@ -53,5 +93,7 @@ module.exports = {
     agregarProductoGET,
     agregarProductoPOST,
     editarProductoGET,
-    loginUsuarioGET
+    loginUsuarioGET,
+    editarProductoPOST,
+    borrarProductoGET
 }
--- ProyectoFinal/routes/back.js
+++ ProyectoFinal/routes/back.js
@@ -1,13 +1,16 @@
 var express = require('express');
 const router = express.Router();
-const { adminGET, agregarProductoGET, editarProductoGET,loginUsuarioGET, agregarProductoPOST} = require('../controllers/back');
+const { adminGET, agregarProductoGET, editarProductoGET,loginUsuarioGET, agregarProductoPOST, editarProductoPOST, borrarProductoGET} = require('../controllers/back');
 
 router.get('/admin', adminGET);
 
 router.get('/agregar-producto', agregarProductoGET);
 router.post('/agregar-producto', agregarProductoPOST);
 
-router.get('/editar-producto', editarProductoGET);
+router.get('/editar-producto/:id', editarProductoGET);
+router.post('/editar-producto/:id', editarProductoPOST);
+
+router.get('/borrar-producto/:id', borrarProductoGET);
 
 router.get('/login', loginUsuarioGET);
 
