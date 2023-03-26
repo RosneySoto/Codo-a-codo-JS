4abca94c146e42bda8a9e471c659e40e9e7c02de
Jose Alvarez <rosney.sotor@gmail.com>
2023-03-26 17:23:36 -0300

    Se modifican variables de entorno y conexion con base dedatos

--- ProyectoFinal/middleware/passport.js
+++ ProyectoFinal/middleware/passport.js
@@ -1,29 +1,29 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
-const { Cuentas } = require('../models/productoDB');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const bcrypt = require('bcrypt');
+// const { Cuentas } = require('../models/productoDB');
 
-passport.use('autenticacion', new LocalStrategy( async (usuario, password, callback) => {
+// passport.use('autenticacion', new LocalStrategy( async (usuario, password, callback) => {
 
-    const usuarioFind = await Cuentas.findOne({ where: { usuario: usuario } });
+//     const usuarioFind = await Cuentas.findOne({ where: { usuario: usuario } });
 
-    if(!usuarioFind) return callback(new Error('USUARIO NO REGISTRADO'));
+//     if(!usuarioFind) return callback(new Error('USUARIO NO REGISTRADO'));
     
-    if(bcrypt.compareSync(password, usuarioFind.password)){
-        return callback(null, usuarioFind);
-    } else {
-        return callback(new Error('ERROR EN LA CONTRASEÑA'), null)
-    }
-}));
+//     if(bcrypt.compareSync(password, usuarioFind.password)){
+//         return callback(null, usuarioFind);
+//     } else {
+//         return callback(new Error('ERROR EN LA CONTRASEÑA'), null)
+//     }
+// }));
 
 
-passport.serializeUser((user,callback) => {
-    callback(null, user.id);
-})
+// passport.serializeUser((user,callback) => {
+//     callback(null, user.id);
+// })
 
-passport.deserializeUser(async (idProducto, callback) => {
-    const user = await Cuentas.findOne({ where: {id: idProducto} });
-    callback(null, user)
-})
+// passport.deserializeUser(async (idProducto, callback) => {
+//     const user = await Cuentas.findOne({ where: {id: idProducto} });
+//     callback(null, user)
+// })
 
-module.exports = passport;
+// module.exports = passport;
