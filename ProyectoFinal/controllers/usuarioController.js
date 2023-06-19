a08cf4a0965be0eee33cac6280e0f4a859895bdb
Jose Alvarez <rosney.sotor@gmail.com>
2023-06-19 17:27:59 -0300

    Valicacion y creacion de token en el usuario

--- ProyectoFinal/controllers/usuarioController.js
+++ ProyectoFinal/controllers/usuarioController.js
@@ -8,78 +8,86 @@ const { tokenSign, tokenVerify } = require('../middleware/auth');
 const adminGET = async (req, res) => {
 
     try {
-        const logueado = req.session.logueado;
 
-        const token = req.headers.authorization.split(' ')[1];
-        const tokenData = await tokenVerify(token);
-        console.log(tokenData);
-        
-        if(Date.now() > tokenData.expiresIn){
-            res.send({error: 'ERROR EN LA VERIFICACION O TOKEN EXPIRADO'});
-        } else {
-            // res.send({message: 'ESTAS EN ADMIN'});
-            if(logueado){
+        if( req.session.logueado === true ){
+            const token = req.headers.authorization.split(' ')[1];
+            const tokenData = await tokenVerify(token);
+
+            if(Date.now() < tokenData.expiresIn){
+                
                 const listaProdutos = await Productos.findAll();
-                res.render('admin', {
-                    titulo: "Vista del Administrador",
-                    logueado: logueado,
-                    usuario: req.session.usuario,
+
+                res.status(200).send({
+                    message: 'ESTAS EN ADMIN',
                     productos: listaProdutos
-                })
+                });
             } else {
-                res.redirect('/login')
-            };
+                res.status(401).send({ message: 'ERROR EN LA VERIFICACION O TOKEN EXPIRADO' });
+            }
+        } else {
+            res.status(401).send({ message: 'ERROR DEBE INICIAR SESION' });
         }
+        
     } catch (error) {
+        res.status(500).send({ error: 'ERROR NO TIENE AUTORIZACION' })
         console.log(error)
     }   
 };
 
 const loginUsuarioGET = (req, res) => {
-    res.render('login', {
-        titulo: "Log In"
-    });
+    res.status(200).send({ message: 'acceso confirmado', titulo: "Log In" });
+    // res.render('login', {
+    //     titulo: "Log In"
+    // });
 };
 
 const loginUsuarioPOST = async (req, res) => {
-    const usuario = req.body.usuario;
-    const clave = req.body.password;
-
+    
     try {
-        const usuarioValidado = await Cuentas.findOne({ where: { usuario: usuario } });
-        const checkPassword = await compare(clave, usuarioValidado.password);
+        const usuario = req.body.usuario;
+        const clave = req.body.password;
 
-        /* TOKEN */
-        const tokenSession = await tokenSign(usuarioValidado);
+        if( !usuario || !clave ){
+            res.status(401).send({
+                error: 'Error: Debe ingresar usuario y contraseña'
+            });
+        } else {
 
-        if (usuario || clave) {
+            const usuarioValidado = await Cuentas.findOne({ where: { usuario: usuario } });
 
-            if (checkPassword) {
-                req.session.logueado = true; //Se crea al iniciar sesion, si no se inicia sesion no se crea.
-                req.session.usuario = usuario;
-                
-                // return res.cookie({"token": token}).json({success:true,message:'LoggedIn Successfully', token: token})
-                // res.redirect('/admin');
-                res.send({
+            if ( usuarioValidado ) {
+
+                const checkPassword = await compare(clave, usuarioValidado.password);
+
+                if(checkPassword){
+
+                    req.session.logueado = true; //Se crea al iniciar sesion, si no se inicia sesion no se crea.
+                    req.session.usuario = usuario;
+                    /* TOKEN */
+                    const tokenSession = await tokenSign(usuarioValidado);
+                    
+                    res.status(200).send({
                     data: usuario,
                     tokenSession
-                })
-
+                    });
+                } else {
+                    res.status(401).send({
+                        error: 'Error: contraseña incorrecta'
+                    });
+                }
             } else {
-                res.render('login', {
-                    titulo: "Login",
-                    error: "Email de usuario o contraseña incorrecta"
+                res.status(401).send({
+                    error: 'Error: Email de usuario incorrecta'
                 });
+                // res.render('login', {
+                //     titulo: "Login",
+                //     error: "Email de usuario o contraseña incorrecta"
+                // });
             };
-
-        } else {
-            res.render('login', {
-                titulo: "Login",
-                error: "Debe ingresar su usuario y password"
-            });
         };
     } catch (error) {
-        console.log('[ERROR]' + error)
+        console.log('[ERROR]' + error);
+        res.status(500).send({ error: 'ERROR INTERNO' })
     };
 };
 
@@ -112,31 +120,32 @@ const registroUsuarioPOST = async (req, res) => {
     try {
         const { usuario, password } = req.body;
 
-        if(!usuario || !password) return res.json({ message: 'NO DEBE HABER CAMPOS VACIOS' })
+        if(!usuario || !password) return res.status(401).send({ message: 'NO DEBE HABER CAMPOS VACIOS' });
         
         const usuarioExiste = await Cuentas.findOne({ where: { usuario: usuario}});
         if( usuarioExiste ){
-            res.send('ERROR EL USUARIO EXISTE')
+            res.status(401).send('ERROR EL USUARIO EXISTE');
         } else {
             let nuevoUsuario;
 
             const passwordHash = await encrypt(password);
-
             const tokenSession = await tokenSign(usuario);
             
             nuevoUsuario = await Cuentas.create({usuario: usuario, password: passwordHash, token: tokenSession});
 
-            res.cookie({ 'token': tokenSession }).json({ success: true, message: 'User registered successfully', data: nuevoUsuario })
-            console.log(nuevoUsuario);
-            
-
-            res.render('registro', {
-                mensaje: 'Cuenta Creada',
-                titulo: 'Sign In'
+            // res.cookie({ 'token': tokenSession }).json({ success: true, message: 'User registered successfully', data: nuevoUsuario })
+            res.status(201).send({
+                message: 'Usuario creado correctamente',
+                data: nuevoUsuario
             });
+            // res.render('registro', {
+            //     mensaje: 'Cuenta Creada',
+            //     titulo: 'Sign In'
+            // });
         };
     } catch (error) {
-        console.log('[ERROR]' + error)
+        res.status(500).send({ error: 'ERROR INTERNO' })
+        console.log('[ERROR]' + error);
     };       
 };
 
